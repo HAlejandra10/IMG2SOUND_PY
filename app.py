12110065d162c951c1d6b3ab9a8d44c6c51a06aa
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-14 11:44:04 -0500

    adding function to add as parameter 'image', create a try to monitore exceptions, adding varible to open image and text variable to extract the content from img and to delete jumps of line with method join() and split(), to print the text and create dictionary w/ object gTTS, keep the audio, to play audio and return True have executed right

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import os
 
 pytesseract.pytesseract.tesseract_cmd= r"C:\Users\heidy\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
+def convert_to_sound(image):
+    
+    try:
+        
+        open_image = Image.open(image)
+        text = pytesseract.image_to_string(open_image)
+        text_file= " ".join(text.split("\n"))
+        print(text_file)
+        sound = gTTS(text_file, lang="en")
+        sound.save("sound.mp3")
+        os.system("sound.mp3")
+        return True
+    
+    except  Exception as bug:
