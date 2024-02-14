c27ba228cea22258150e98c0bdcf0cb7057a4356
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-14 11:28:42 -0500

    create enviromentvirtual and add file called app.py and img too

---
+++ app.py
@@ -0,0 +1,7 @@
+from PIL import Image
+from gtts import gTTS 
+import pytesseract
+import os
+
+pytesseract.pytesseract.tesseract_cmd= r"C:\Users\heidy\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
